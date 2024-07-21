c8d994bef46cc7ebbb1f9765515a0ee0300b8ca9
Sneha0323 <137058731+Sneha0323@users.noreply.github.com>
2024-07-21 15:46:02 +0530

    changes

--- src/schemas/acceptMessageSchema.ts
+++
@@ -1,5 +0,0 @@
-import {z} from "zod"
-
-export const acceptMessegesSchema = z.object({
-    acceptMesseges: z.boolean(),
-})
--- src/schemas/verifySchema.ts
+++
@@ -1,5 +0,0 @@
-import {z} from "zod"
-
-export const verifySchema = z.object({
-    code: z.string().length(8, 'Verification code must be 8 digit')
-})
